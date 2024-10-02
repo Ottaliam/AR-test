fd2998ab9a078d2f3c9fa4246ddd96059bb492e3
Ottaliam <ottaliam@gmail.com>
2024-10-02 00:47:00 -0400

    Test button click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const redButton = document.querySelector('#redButton');
 const greenButton = document.querySelector('#greenButton');
 const blueButton = document.querySelector('#blueButton');
+const grayButton = document.querySelector('#grayButton');
 const table = document.querySelector('#table');
 const buttonContainer = document.querySelector('.button-container');
 
@@ -12,13 +13,6 @@ const changeModelColor = (color) => {
     });
 };
 
-const colors = ['red', 'green', 'blue'];
-let colorIndex = 0;
-const toggleModelColor = () => {
-    colorIndex = (colorIndex + 1) % colors.length;
-    changeModelColor(colors[colorIndex]);
-}
-
 redButton.addEventListener('click', () => {
     changeModelColor('red');
 });
@@ -28,39 +22,7 @@ greenButton.addEventListener('click', () => {
 blueButton.addEventListener('click', () => {
     changeModelColor('blue');
 });
-table.addEventListener('click', () => {
-    toggleModelColor();
-})
 
-
-let longPressTimer;
-let longPressTriggered = false;
-const longPressThreshold = 500;
-
-const onPressStart = (e) => {
-    longPressTriggered = false;
-    longPressTimer = setTimeout(() => {
-        longPressTriggered = true;
-        document.querySelector('.button-container').style.visibility = 'visible';
-    }, longPressThreshold);
-}
-
-const onPressEnd = (e) => {
-    clearTimeout(longPressTimer);
-    if (!longPressTriggered) {
-        toggleModelColor();
-    }
-}
-
-const onPressCancel = () => {
-    clearTimeout(longPressTimer);
-}
-
-document.addEventListener('touchstart', onPressStart);
-document.addEventListener('touchend', onPressEnd);
-document.addEventListener('touchmove', onPressCancel);
-document.addEventListener('touchcancel', onPressCancel);
-
-document.addEventListener('mousedown', onPressStart);
-document.addEventListener('mouseup', onPressEnd);
-document.addEventListener('mouseleave', onPressCancel);
+grayButton.addEventListener('click', () => {
+    buttonContainer.toggleAttribute('hidden');
+});
